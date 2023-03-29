d0a8897398b507bb442fca36a89581e59f5566ed
林思恺 <linsk.vip@qq.com>
2023-03-29 18:24:40 +0800

    增加amd配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,19 @@ export default {
 	input:"index.js",
 	output:[{
 		file:"dist/simple-date-format.js",
+		interop: "esModule",
+		esModule: true,
+		exports: 'named',
+		format:"umd",
 		name:"SimpleDateFormat",
-		format:"umd"
+		amd: {
+			id: 'java.text.simple-date-format'
+		}
 	},{
 		file:"dist/simple-date-format.mjs",
+		interop: "esModule",
+		esModule: true,
+		exports: 'named',
 		format:"esm"
 	}],
 	plugins:[
